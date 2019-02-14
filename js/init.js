411a82da186413329cb77fd81929f8a1c29973f5
MrcSantos <mirco.santosuosso@gmail.com>
2019-02-14 14:29:46 +0100

    After merge

--- js/init.js
+++ js/init.js
@@ -1,3 +1,40 @@
+var app = {
+	/**
+	 * Application Constructor, fires the deviceReady event
+	 * */
+	initialize: function () {
+		document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+	},
+
+	/**
+	 ** The app starts here, after the deviceReady event
+	 */
+	onDeviceReady: function () {
+		init();
+		startApp();
+	}
+};
+
+//--------------------------------------------------// Things to do before the app runs
+
+/**
+ * Things to do before the app runs
+ */
+function init() {
+	// TODO Initialize gui and Vue
+	dbg.status = 'Waiting for bluetooth...';
+}
+
+//--------------------------------------------------// Initialization of the Vue instances + Cordova app
+
+var dbg = new Vue({
+	el: '#dbg',
+	data: {
+		status: 'App loading...',
+		connection: 'App loading...'
+	}
+})
+
 const gui = new Vue({
 	el: '#gui',
 
@@ -8,3 +45,6 @@ const gui = new Vue({
 		devices: []
 	}
 });
+
+
+app.initialize();
